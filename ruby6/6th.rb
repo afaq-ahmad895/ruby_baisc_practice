652a22a25d56834f9ba3cad8114ba7d3e05eadd4
Afaq <affakahmad785@gmail.com>
2023-03-14 19:10:37 +0500

    basic ruby

--- ruby6/6th.rb
+++ ruby6/6th.rb
@@ -1,7 +1,14 @@
 # 15 - Write a program that takes a string as input and returns the string with all the consonants removed.
 
-def remove_consonants()
-string = "Hello, world!"
-new_string = remove_consonants(string)
-puts new_string 
+def remove_consonants(str)
+  vowels = ['a', 'e', 'i', 'o', 'u']
+  result = ''
+
+  str.each_char do |c|
+    result += c if vowels.include?(c.downcase)
+  end
+
+  return result
 end
+
+puts remove_consonants("hello world")  
